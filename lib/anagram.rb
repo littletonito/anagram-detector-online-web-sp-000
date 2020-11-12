5d506a42e703be5ca9e7c14b6ada09a1ddea4ab3
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-11-12 22:29:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.map do |w|
+      w.split(" ").sort == @word.split(" ").sort
+
+
+    end
+  end
+
+
+
+
+
+end
